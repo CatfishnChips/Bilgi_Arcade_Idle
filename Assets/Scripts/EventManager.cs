a5adc8c730b321782d1b23caa7213426ecddcac8
CatfishnChips <denizpoyraz12@hotmail.com>
2022-04-20 17:57:47 +0300

    Homework

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -19,11 +20,60 @@ public class EventManager : MonoBehaviour
 
     #endregion
 
+    #region UnityEvents
+
     public UnityAction<Vector2> onInputDragged = delegate { };
     public UnityAction onInputReleased = delegate { };
 
-    //public event UnityAction onInputReleased;
-    // Alternative way of doing events.
+    // ALTERNATIVE, WE DO NOT NEED TO CREATE AN EMPTY DELEGATE
+    //public UnityAction<Vector2> onInputDragged = (Vector2 vector2) => { };
+    //public UnityAction onInputReleased = () => { };
 
-    // Never do Static events, even tho the object is destoryed they remain in the project until it is closed. Only do Singleton pattern for events.
+    // If we are already checking for null whilst invoking the action, I don't we need to set the action to a null delegate.
+
+    // ONE !!! THIS IS A WRONG WAY, BECAUSE EVENTS CANNOT BE INVOKED FROM ANOTHER CLASS !!!
+    //public UnityEvent<Vector2> onInputDragged;
+    //public UnityEvent onInputReleased;
+
+    // TWO !!! THIS IS A WRONG WAY, BECAUSE EVENTS CANNOT BE INVOKED FROM ANOTHER CLASS !!!
+    //public event UnityAction<Vector2> onInputDragged;
+    //public event UnityAction onInputReleased;
+
+    #endregion 
+
+    #region C# Events
+
+    // ONE C# WAY OF DOING ACTIONS
+    //public Action<Vector2> onInputDragged;
+    //public Action onInputReleased;
+
+    // TWO C# WAY OF CREATING MANUAL DELEGATES
+    //public delegate void OnInputDragged(Vector2 vector);
+    //public OnInputDragged onInputDragged;
+
+    //public delegate void OnInputReleased();
+    //public OnInputReleased onInputReleased;
+
+    // THREE !!! THIS IS A WRONG WAY, BECAUSE EVENTS CANNOT BE INVOKED FROM ANOTHER CLASS !!!
+    //public delegate void OnInputDragged(Vector2 vector);
+    //public event OnInputDragged onInputDragged;
+
+    //public delegate void OnInputReleased();
+    //public event OnInputReleased onInputReleased;
+
+    // FOUR !!! THIS IS A WRONG WAY, BECAUSE IT REQUIRES AN OBJECT REFERENCE AND ALSO IS AN EVENT !!! 
+    //public EventHandler<Vector2> onInputDragged;
+    //public EventHandler onInputReleased;
+
+    // FIVE !!! THIS IS A WRONG WAY, BECAUSE FUNC HAS A RETURN TYPE AND WE DO NOT NEED A RETURN TYPE !!!
+    // Func instead of Action
+
+    #endregion 
+
+    // NOTES
+    
+    // If the object is temporary:
+    // Never do Static events, even tho the object is destroyed they remain in the project until it is closed. Only do Singleton pattern for events.
+
+    // To use Events, both EventManager and InputManager should be in one class OR InputManager should have its own input Events.
 }
--- Assets/Scripts/PlayerPhysicsController.cs
+++ Assets/Scripts/PlayerPhysicsController.cs
@@ -18,6 +18,7 @@ public class PlayerPhysicsController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other.name);
         if (other.CompareTag("Obstacle"))
         {
             other.gameObject.SetActive(false);
